ad0708f045a7e71ef20dd1ac8ba92084b3a6b060
xizi <xizi@DESKTOP-3ST1KL0>
2017-12-12 09:24:55 +0800

    20171212

--- NFramework/NFramework.Common/EncryptCore.cs
+++ NFramework/NFramework.Common/EncryptCore.cs
@@ -30,81 +30,135 @@ namespace NFramework.Common
             return encryptResult;
         }
 
+
+        #region 密钥和向量
+        private static byte[] iv = { 89,33, 12, 34, 45, 56, 67, 78, 89, 91, 102, 113, 124, 135, 146, 157 };
+        private static byte[] key = { 2, 21, 28, 73, 47, 95, 96, 79, 98, 93, 102, 114, 251, 133, 142, 157 };
+        private static CipherMode cipherMode = CipherMode.CBC;
+        private static PaddingMode paddingMode = PaddingMode.PKCS7;
+        #endregion
+
+        #region AES加密
         /// <summary>
-        /// 有密码的AES加密 
+        /// AES字符串加密
         /// </summary>
-        /// <param name="text">加密字符</param>
-        /// <param name="key">加密的密码</param>
-        /// <param name="iv">密钥</param>
+        /// <param name="text"></param>
         /// <returns></returns>
-        public static string AESEncrypt(string text, string key, string iv)
+        public static string Encrypt(string text)
         {
-            RijndaelManaged rijndaelCipher = new RijndaelManaged();
-            rijndaelCipher.Mode = CipherMode.CBC;
-            rijndaelCipher.Padding = PaddingMode.PKCS7;
-            rijndaelCipher.KeySize = 128;
-            rijndaelCipher.BlockSize = 128;
-            byte[] pwdBytes = System.Text.Encoding.UTF8.GetBytes(key);
-            byte[] keyBytes = new byte[16];
-            int len = pwdBytes.Length;
-            if (len > keyBytes.Length) len = keyBytes.Length;
-            System.Array.Copy(pwdBytes, keyBytes, len);
-            rijndaelCipher.Key = keyBytes;
-            byte[] ivBytes = System.Text.Encoding.UTF8.GetBytes(iv);
-            rijndaelCipher.IV = ivBytes;
-            ICryptoTransform transform = rijndaelCipher.CreateEncryptor();
-            byte[] plainText = Encoding.UTF8.GetBytes(text);
-            byte[] cipherBytes = transform.TransformFinalBlock(plainText, 0, plainText.Length);
-            return Convert.ToBase64String(cipherBytes);
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            byte[] release = AESEncrypt(data, iv, key, cipherMode, paddingMode);
+            return Convert.ToBase64String(release);
         }
 
         /// <summary>
-        /// 随机生成密钥
+        /// AES字符串加密
         /// </summary>
+        /// <param name="text"></param>
         /// <returns></returns>
-        public static string GetIv(int n)
+        public static string Encrypt(string text, byte[] keys)
         {
-            char[] arrChar = new char[]{
-                                       'a','b','d','c','e','f','g','h','i','j','k','l','m','n','p','r','q','s','t','u','v','w','z','y','x',
-                                       '0','1','2','3','4','5','6','7','8','9',
-                                       'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Q','P','R','T','S','V','U','W','X','Y','Z'
-                                      };
-            StringBuilder num = new StringBuilder();
-            Random rnd = new Random(DateTime.Now.Millisecond);
-            for (int i = 0; i < n; i++)
+            var _iv = new byte[16];
+            var _key = new byte[16];
+            Array.Copy(keys, 0, _iv, 0, 16);
+            Array.Copy(keys, 16, _key, 0, 16);
+
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            byte[] release = AESEncrypt(data, _iv, _key, cipherMode, paddingMode);
+            return Convert.ToBase64String(release);
+        }
+
+        /// <summary>
+        /// AES加密
+        /// </summary>
+        /// <param name="inputdata">输入的数据</param>
+        /// <param name="iv">向量128位</param>
+        /// <param name="strKey">加密密钥</param>
+        /// <returns></returns>
+        public static byte[] AESEncrypt(byte[] inputdata, byte[] iv, byte[] key, CipherMode cipherMode, PaddingMode paddingMode)
+        {
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            //设置密钥及密钥向量
+            aes.Key = key;
+            aes.IV = iv;
+            aes.Mode = cipherMode;
+            aes.Padding = paddingMode;
+
+            ICryptoTransform transform = aes.CreateEncryptor();
+            byte[] data = transform.TransformFinalBlock(inputdata, 0, inputdata.Length);
+            aes.Clear();
+            return data;
+
+        }
+        #endregion
+
+        #region AES解密
+        /// <summary>
+        /// AES字符串解密
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string Decrypt(string text)
+        {
+            try
             {
-                num.Append(arrChar[rnd.Next(0, arrChar.Length)].ToString());
+                byte[] data = Convert.FromBase64String(text);
+                byte[] release = AESDecrypt(data, iv, key, cipherMode, paddingMode);
+                return Encoding.UTF8.GetString(release);
             }
-            return num.ToString();
+            catch (Exception e)
+            {
+                throw new Exception("错误的密文");
+            }
+
+        }
+
+        /// <summary>
+        /// AES字符串解密
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string Decrypt(string text, byte[] keys)
+        {
+            var _iv = new byte[16];
+            var _key = new byte[16];
+            Array.Copy(keys, 0, _iv, 0, 16);
+            Array.Copy(keys, 16, _key, 0, 16);
+
+            try
+            {
+                byte[] data = Convert.FromBase64String(text);
+                byte[] release = AESDecrypt(data, _iv, _key, cipherMode, paddingMode);
+                return Encoding.UTF8.GetString(release);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("错误的密文");
+            }
+
         }
 
         /// <summary>
         /// AES解密
         /// </summary>
-        /// <param name="text"></param>
-        /// <param name="password"></param>
-        /// <param name="iv"></param>
+        /// <param name="inputdata">输入的数据</param>
+        /// <param name="iv">向量128</param>
+        /// <param name="strKey">key</param>
         /// <returns></returns>
-        public static string AESDecrypt(string text, string key, string iv)
+        public static byte[] AESDecrypt(byte[] inputdata, byte[] iv, byte[] key, CipherMode cipherMode, PaddingMode paddingMode)
         {
-            RijndaelManaged rijndaelCipher = new RijndaelManaged();
-            rijndaelCipher.Mode = CipherMode.CBC;
-            rijndaelCipher.Padding = PaddingMode.PKCS7;
-            rijndaelCipher.KeySize = 128;
-            rijndaelCipher.BlockSize = 128;
-            byte[] encryptedData = Convert.FromBase64String(text);
-            byte[] pwdBytes = System.Text.Encoding.UTF8.GetBytes(key);
-            byte[] keyBytes = new byte[16];
-            int len = pwdBytes.Length;
-            if (len > keyBytes.Length) len = keyBytes.Length;
-            System.Array.Copy(pwdBytes, keyBytes, len);
-            rijndaelCipher.Key = keyBytes;
-            byte[] ivBytes = System.Text.Encoding.UTF8.GetBytes(iv);
-            rijndaelCipher.IV = ivBytes;
-            ICryptoTransform transform = rijndaelCipher.CreateDecryptor();
-            byte[] plainText = transform.TransformFinalBlock(encryptedData, 0, encryptedData.Length);
-            return Encoding.UTF8.GetString(plainText);
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            aes.Key = key;
+            aes.IV = iv;
+            aes.Mode = cipherMode;
+            aes.Padding = paddingMode;
 
+            ICryptoTransform transform = aes.CreateDecryptor();
+            byte[] data = null;
+            data = transform.TransformFinalBlock(inputdata, 0, inputdata.Length);
+            aes.Clear();
+            return data;
         }
+        #endregion
     }
 }
---
+++ NFramework/NFramework.Common/StringHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFramework.Common
+{
+    /***************************************************
+    * Author  :jony
+    * Date    :2017-12
+    * Describe:定义一个字符串处理类
+    * 
+    * 
+    ***************************************************/
+    public class StringHelper
+    {
+        /// <summary>
+        /// 获取一个随机字符串
+        /// </summary>
+        /// <param name="len">字符串长度</param>
+        /// <returns></returns>
+        public static string GetRandomString(int len)
+        {
+            var str = new TempString();
+            return str.GenerateCheckCode(len);
+        }
+    }
+
+    internal class TempString
+    {
+        private int rep = 0;
+        /// 
+        /// 生成随机字母字符串(数字字母混和)
+        /// 
+        /// 待生成的位数
+        /// 生成的字母字符串
+        public string GenerateCheckCode(int codeCount)
+        {
+            string str = string.Empty;
+            long num2 = DateTime.Now.Ticks + this.rep;
+            this.rep++;
+            Random random = new Random(((int)(((ulong)num2) & 0xffffffffL)) | ((int)(num2 >> this.rep)));
+            for (int i = 0; i < codeCount; i++)
+            {
+                char ch;
+                int num = random.Next();
+                if ((num % 2) == 0)
+                {
+                    ch = (char)(0x30 + ((ushort)(num % 10)));
+                }
+                else
+                {
+                    ch = (char)(0x41 + ((ushort)(num % 0x1a)));
+                }
+                str = str + ch.ToString();
+            }
+            return str;
+        }
+    }
+}
