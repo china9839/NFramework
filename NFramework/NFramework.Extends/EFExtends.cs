ed9ba7cb4c8f471029f584d8caa4f7bbaf3a3d08
xizi <xizi@DESKTOP-3ST1KL0>
2018-04-08 15:00:23 +0800

    ef extend BulkInsert

--- NFramework/NFramework.Extends/EFExtends.cs
+++ NFramework/NFramework.Extends/EFExtends.cs
@@ -1,8 +1,11 @@
 ﻿using NFramework.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -88,6 +91,48 @@ namespace NFramework.Extends
                     }
                 }
                 return -1000;
+            }
+        }
+
+        /// <summary>  
+        /// EF批量插入 ,只支持sqlserver
+        /// </summary>  
+        /// <typeparam name="T">泛型集合的类型</typeparam>  
+        /// <param name="conn">连接对象</param>  
+        /// <param name="tableName">将泛型集合插入到本地数据库表的表名</param>  
+        /// <param name="list">要插入大泛型集合</param>  
+        public static void BulkInsert<T>(this DbContext dbContext,string tableName, IList<T> list)
+        {
+            var connection = dbContext.Database.Connection as SqlConnection;
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open(); //打开Connection连接  
+            }
+            using (var bulkCopy = new SqlBulkCopy(connection))
+            {
+                bulkCopy.BatchSize = list.Count;
+                bulkCopy.DestinationTableName = tableName;
+                var table = new DataTable();
+                var props = TypeDescriptor.GetProperties(typeof(T))
+                    .Cast<PropertyDescriptor>()
+                    .Where(propertyInfo => propertyInfo.PropertyType.Namespace.Equals("System"))
+                    .ToArray();
+                foreach (var propertyInfo in props)
+                {
+                    bulkCopy.ColumnMappings.Add(propertyInfo.Name, propertyInfo.Name);
+                    table.Columns.Add(propertyInfo.Name, Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType);
+                }
+                var values = new object[props.Length];
+                foreach (var item in list)
+                {
+                    for (var i = 0; i < values.Length; i++)
+                    {
+                        values[i] = props[i].GetValue(item);
+                    }
+
+                    table.Rows.Add(values);
+                }
+                bulkCopy.WriteToServer(table);
             }
         }
     }
